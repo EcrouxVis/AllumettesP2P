52404d1f032217a920e296feb33a9881a671141d
Abdou <abdouzozo.14@gmail.com>
2020-03-03 16:32:22 +0100

    Main test

--- src/com/ecrouxvis/fr/Main.java
+++ src/com/ecrouxvis/fr/Main.java
@@ -1,8 +1,36 @@
 package com.ecrouxvis.fr;
 
+import com.ecrouxvis.fr.Controller.GameController;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World!");
+        //debut du programme
+        System.out.println("Binevenue dans la partie : ");
+        Scanner sc = new Scanner(System.in);
+
+        GameController gameController = new GameController();
+        //creation de la partie ...
+        gameController.creerPartie();
+        gameController.setIpVoisin("127.0.0.0");
+        int nbAllumDem;
+        System.out.println("Il y a " + gameController.getNbAllumettes() + " allumettes");
+
+        while(true){
+            do {
+                System.out.println("Combien d'allumettes voulez vous enlever : 1, 2, ou 3 : ");
+                nbAllumDem = sc.nextInt();
+            }while(nbAllumDem<1 || nbAllumDem>3);
+
+            System.out.println("Il reste "+ gameController.enleverAllumettes(nbAllumDem) +" allumettes");
+        }
+
+
+
+
+
+
     }
 }
