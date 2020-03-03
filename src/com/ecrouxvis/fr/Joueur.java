59dd40ef76de3add8d4b5955c7668b9db3511fb4
Abdou <abdouzozo.14@gmail.com>
2020-03-03 11:24:14 +0100

    Ajout partie et joueur (classe)

---
+++ src/com/ecrouxvis/fr/Joueur.java
@@ -0,0 +1,22 @@
+public class Joueur {
+    private int id;
+    private string ipAdress;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public string getIpAdress() {
+        return ipAdress;
+    }
+
+    public void setIpAdress(string ipAdress) {
+        this.ipAdress = ipAdress;
+    }
+
+
+}
