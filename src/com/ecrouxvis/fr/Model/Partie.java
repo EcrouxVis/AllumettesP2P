dee24c2a7239aa597a635ba4faaaedc74a2158d4
Pointet Nicolas <nicolas.91.pointet@hotmail.fr>
2020-03-03 14:17:11 +0100

    CreationClassePartie_3/...

--- src/com/ecrouxvis/fr/Model/Partie.java
+++ src/com/ecrouxvis/fr/Model/Partie.java
@@ -1,75 +1,74 @@
 package com.ecrouxvis.fr.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Partie {
-    private int nbAllumette;
-    private int tour = 0;
-    private int maxAlumette;
-    private int nbJoueur;
-    private int dernier_joueur;
+    private int nbAllumette;                     //nombre d'allumette de la partie
+    private int nbJoueur;                        //nombre  de joueur de la partie
 
-    public int getNbAllumette() {
-        return nbAllumette;
+    private List<Joueur> joueur = new ArrayList<Joueur>();      //liste des joueurs de la manche
+    private List<Joueur> spect = new ArrayList<Joueur>();       //liste des spectateurs de la manche (les joueurs ayant perdus)
+
+
+    public boolean addJoueur (Joueur e) { return joueur.add(e); }                   //permet d'ajouter un joueur á la partie
+
+    public int getNbAllumette() { return this.nbAllumette; }                        //renvoie le nombre d'allumette de la manche
+
+    public void removeAllumette(int nombre) { this.nbAllumette -= nombre; }         //permet d'enlever un certain nombre d'allumette
+
+    public void calculNbAllumette() {this.nbAllumette = this.nbJoueur * 6 + 1;}     //initialise le bon nombre d'allumette pour la manche a jouer
+
+    public int getNbJoueur() { return nbJoueur; }                                   //renvoie le nombre de joueur de la manche
+
+    public void calculNbJoueur() { this.nbJoueur = this.joueur.size(); }            //permet de calculer le nombre de joueur de la manche
+
+
+    Partie(){
+
     }
 
-    public void setNbAllumette(int nbAllumette) {
-        this.nbAllumette = nbAllumette;
-    }
+    Partie (List<Joueur> joueurs, List<Joueur> spects) {
 
-    public int getTour() {
-        return tour;
-    }
-
-    public void setTour(int tour) {
-        this.tour = tour;
-    }
-
-    public int getMaxAlumette() {
-        return maxAlumette;
-    }
-
-    public void setMaxAlumette(int maxAlumette) { this.maxAlumette = maxAlumette; }
-
-    public int getNbJoueur() { return nbJoueur; }
-
-    public void setNbJoueur(int nbJoueur) { this.nbJoueur = nbJoueur; }
-
-    public int getDernier_joueur() { return dernier_joueur; }
-
-    public void setDernier_joueur(int dernier_joueur) { this.dernier_joueur = dernier_joueur; }
-
-    public int check_etat(){
-        //return null si la partie est encore en cours
-        //return id du joueur perdant si la partie est finie
-        if(nbAllumette==0){
-            return dernier_joueur;
-        }
-        else
-            return 1; //return id
-    }
-
-    Joueur Partie (List<Joueur> joueur, List<Joueur> spect) {   //return le joueur vainqueur
+        this.joueur = joueurs;
+        this.spect = spects;
 
         this.nbJoueur = joueur.size();
 
         this.nbAllumette = this.nbJoueur * 6 + 1;
+    }
+
+    public Joueur jouerManche (){                   //retourne le joueur vainqueur de la manche
+
+        calculNbJoueur();                           //met a jour le nombre de joueur pour cette manche
+        calculNbAllumette();                        //met a jour le nombre d'allumette pour cette manche
 
         if (this.nbJoueur == 2){
+
+
+            //manche a 2 joueurs
+
+
+
             Joueur vainqueur = null;
-
-            //partie a 2 joueurs
-
             return  vainqueur;
         }
 
+
+        //la manche a au moins 3 joueurs
+
+
+
+
+
+        //perdant = joueur ayant perdu
         Joueur perdant = null;
-        //partie a au moins 3 joueurs
-        // id_perdant = id du joueur ayant perdu
+        this.joueur.remove(perdant);                //le joueur qui a perdu est enleve de la liste des participants
+        this.spect.add(perdant);                    //le joueur qui a perdu est ajoute a la liste de spectateur
 
-        joueur.remove(perdant);              //le joueur qui a perdu est enleve de la liste des participants
-        spect.add(perdant);                  //le joueur qui a perdu est ajoute a la liste de spectateur
-
-        return Partie(joueur,spect);            //on lance une autre partie sans le joueur qui a perdu
+        return null;                                //il n'y a pas encore de vainqueur
     }
+
+
+
 }
